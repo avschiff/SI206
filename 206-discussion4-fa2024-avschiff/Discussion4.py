6b6687f5ff903c4eb50e095100199f65e25363bf
Avery Schiff <averyschiff@Averys-MacBook-Pro-4.local>
2024-09-18 17:06:30 -0400

    first function

--- 206-discussion4-fa2024-avschiff/Discussion4.py
+++ 206-discussion4-fa2024-avschiff/Discussion4.py
@@ -2,10 +2,9 @@ import math
 
 class Rectangle():
     # Create the constructor "__init__" method
-
-    # YOUR CODE HERE
-
-
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     # Create the "__str__" method
 
