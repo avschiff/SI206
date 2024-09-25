97e4d0f390e24f86f4bed79aa847c14e893772fd
Avery Schiff <averyschiff@Averys-MacBook-Pro-4.local>
2024-09-25 15:55:58 -0400

    fourth

--- 206-discussion5-fall2024-avschiff/discussion_5.py
+++ 206-discussion5-fall2024-avschiff/discussion_5.py
@@ -108,8 +108,8 @@ class TestAllMethods(unittest.TestCase):
 
 	# Check to see whether the warehouse correctly return the item with the highest price
 	def test_warehouse_max_price(self):
-		w3 = Warehouse()
-		pass
+    	w3 = Warehouse([self.item1, self.item2, self.item3, self.item4, self.item5])
+        self.assertEqual(w3.get_max_price(), self.item1)
 		
 
 def main():
