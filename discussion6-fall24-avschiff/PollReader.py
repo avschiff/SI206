6109dd6bdf88dd0c393f8024442fea91a58196e5
Avery Schiff <averyschiff@Averys-MacBook-Pro-4.local>
2024-10-02 17:11:22 -0400

    commit 2

--- discussion6-fall24-avschiff/PollReader.py
+++ discussion6-fall24-avschiff/PollReader.py
@@ -58,13 +58,13 @@ class PollReader():
         for i in self.raw_data:
 
             # split up the row by column
-            seperated = i.split(' ')
+            seperated = i.split(',')
 
             # map each part of the row to the correct column
             self.data_dict['month'].append(seperated[0])
             self.data_dict['date'].append(int(seperated[1]))
             self.data_dict['sample'].append(int(seperated[2]))
-            self.data_dict['sample type'].append(seperated[2])
+            self.data_dict['sample type'].append(seperated[3])
             self.data_dict['Harris result'].append(float(seperated[3]))
             self.data_dict['Trump result'].append(float(seperated[4]))
 
@@ -80,7 +80,15 @@ class PollReader():
             str: A string indicating the candidate with the highest polling percentage or EVEN,
              and the highest polling percentage.
         """
-        pass
+        harris_max = max(self.data_dict['Harris result'])
+        trump_max = max(self.data_dict['Trump result'])
+
+        if harris_max > trump_max:
+            return f"Harris: {harris_max:.2f}%"
+        elif trump_max > harris_max:
+            return f"Trump: {trump_max:.2f}%"
+        else:
+            return f"{harris_max:.2f}% and {trump_max:.2f}% are EVEN"
 
 
     def likely_voter_polling_average(self):
